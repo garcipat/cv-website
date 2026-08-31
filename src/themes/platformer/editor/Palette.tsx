3c94e9fdb213b02a1756920310fc0145ade07fd9
Patrick Garcia <info@pgarcia.dev>
2026-08-31 10:05:08 +0200

    feat(platformer): add Palette component for tool selection

---
+++ src/themes/platformer/editor/Palette.tsx
@@ -0,0 +1,38 @@
+import { TERRAIN_CHARS, ENTITY_CHARS } from '../level/LevelParser';
+import type { TileChar } from '../level/LevelParser';
+
+interface PaletteProps {
+  selectedTool: TileChar;
+  onSelectTool: (tool: TileChar) => void;
+}
+
+const EMPTY_CHAR: TileChar = '.';
+
+export const Palette = ({ selectedTool, onSelectTool }: PaletteProps) => {
+  const terrainKeys = (Object.keys(TERRAIN_CHARS) as TileChar[]).filter(
+    (key) => key !== EMPTY_CHAR,
+  );
+  const entityKeys = Object.keys(ENTITY_CHARS) as TileChar[];
+
+  return (
+    <div role="toolbar">
+      {[...terrainKeys, ...entityKeys].map((key) => (
+        <button
+          key={key}
+          type="button"
+          aria-pressed={selectedTool === key}
+          onClick={() => onSelectTool(key)}
+        >
+          {key}
+        </button>
+      ))}
+      <button
+        type="button"
+        aria-pressed={selectedTool === EMPTY_CHAR}
+        onClick={() => onSelectTool(EMPTY_CHAR)}
+      >
+        Eraser
+      </button>
+    </div>
+  );
+};
